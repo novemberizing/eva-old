2d556f00c685621955c66759d599c5c106d225ed
novemberizing <novemberizing@gmail.com>
2021-04-04 15:54:48 +0900

    update string benchmark.

--- src/example/benchmark/string.c
+++ src/example/benchmark/string.c
@@ -18,28 +18,36 @@ static void init(void)
     buffer[8192] = 0;
 }
 
-static void copy(char * __restrict__ dest, char * __restrict__ src, int n)
+static void copy(char * __restrict__ dest, char * __restrict__ src, int n, int out)
 {
-    n = n / 4;
-    printf("total => %d\n", n);
+    register unsigned long * desti = (unsigned long *) dest;
+    register unsigned long * srci = (unsigned long *) src;
+
+    register unsigned long to = n / 16;
+    
     // register __int128_t * desti = (__int128_t *) dest;
     // register __int128_t * srci = (__int128_t *) src;
-    register int * desti = (int *) dest;
-    register int * srci = (int *) src;
 
     xtime start = xtimeget();
-    while(n--)
+    while(to--)
     {
-        *(desti + n) = *(srci + n);
+        *(desti + to) = *(srci + to);
+//        *(desti + n) = *(srci + n);
     }
+    // for(xint64 i = 0; i < n; i++)
+    // {
+    //     *(desti + i) = *(srci + i);
+    // }
     xtime end = xtimeget();
 
     xtime diff = xtimediff(xaddressof(end), xaddressof(start));
-
-    printf("%ld.%09ld\n", diff.second, diff.nanosecond);
+    if(out)
+    {
+        printf("%ld.%09ld\n", diff.second, diff.nanosecond);
+    }
 }
 
-static void memcopy(char * dest, char * src, int n)
+static void memcopy(char * __restrict__ dest, char * __restrict__ src, int n)
 {
     xtime start = xtimeget();
 
@@ -56,27 +64,44 @@ char dest2[10000];
 
 int main(int argc, char ** argv)
 {
+
     memset(dest2, 0, 8192);
-    memcopy(dest2, buffer, 8192);
+    memcpy(dest2, buffer, 8192);
 
     init();
 
-    memset(dest2, 0, 8192);
-    memcopy(dest2, buffer, 8192);
-    
-    if(memcmp(dest2, buffer, 8192) != 0)
+    for(int i = 0; i < 32; i++)
     {
-        printf("fail\n");
+        // memset(dest2, 0, 8192);
+        memcopy(dest2, buffer, 8192);
+        
+        // if(memcmp(dest2, buffer, 8192) != 0)
+        // {
+        //     printf("fail\n");
+        // }
     }
 
+    printf("===========================\n");
 
+    copy(dest, buffer, 8192, 0);
 
-    memset(dest, 0, 8192);
-    copy(dest, buffer, 8192);
-    if(memcmp(dest, buffer, 8192) != 0)
+    for(int i = 0; i < 32; i++)
     {
-        printf("fail\n");
+        // memset(dest, 0, 8192);
+        copy(dest, buffer, 8192, 1);
+        // if(memcmp(dest, buffer, 8192) != 0)
+        // {
+        //     printf("fail\n");
+        // }
+
+        // printf("===========================\n");
+
+
+
+
+
     }
+
 
 
     return 0;
